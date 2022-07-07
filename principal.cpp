fa5ff4ee38f13566937f3f6b4047a7fbeabb0657
Danizet <lproanoc1@est.ups.edu.ec>
2022-07-07 11:57:13 -0500

    Aplicacion terminada

--- principal.cpp
+++ principal.cpp
@@ -13,3 +13,44 @@ Principal::~Principal()
     delete ui;
 }
 
+
+void Principal::on_cmdCalcular_released()
+{
+    calcular();
+}
+
+void Principal::on_action_Calcular_triggered()
+{
+    calcular();
+}
+
+void Principal::calcular()
+{
+    double num1 = ui->inNum1->value();
+    double num2 = ui->inNum2->value();
+
+    double resultado = 0.0;
+    //Aplicacion terminada commit
+
+    switch (ui->inOperacion->currentIndex()) {
+    case 0:
+        resultado = num1 + num2;
+        break;
+    case 1:
+        resultado = num1 - num2;
+        break;
+    case 2:
+        resultado = num1 * num2;
+        break;
+    case 3:
+        resultado = num1 / num2;
+    default:
+        break;
+    }
+
+    QMessageBox::information(this,
+                             "Calculo",
+                             "El resultado es: " +
+                             QString::number(resultado));
+
+}
